059b9241268f1532a535b26b05b022f7d63b5673
Jaco Bezuidenhout <jaco@peoplesoft.co.za>
2014-07-28 21:24:00 +0000

    added phpMyAdmin; updated steps.php

--- steps.php
+++ steps.php
@@ -6,7 +6,10 @@
 	echo "<li>apt-get install apache2</li>";
 	echo "<li>apt-get install mysql-server</li>";
 	echo "<li>apt-get install php5</li>";
+	echo "<li>apt-get install php5-mysql</li>";
 	echo "<li>a2enmod php5</li>";
+	echo "<li>nano /etc/php5/apache2/php.ini</li>";
+	echo "<small>( added extension=mysql.so )</small>"
 	echo "<li>echo 'echo php_info();' > /var/www/index.php</li>";
 
 ?>
